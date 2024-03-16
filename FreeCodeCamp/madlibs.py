71c08d1d9d03d09391f301d251cd04a3bf25210c
hfbw03 <126973082+hfbw03@users.noreply.github.com>
2024-03-17 00:20:48 +0800

    Add files via upload

---
+++ FreeCodeCamp/madlibs.py
@@ -0,0 +1,11 @@
+#madlibs.py
+
+topic = input("Topic: ")
+person = input("Person: ")
+another_topic = input("Another Topic: ")
+adjective = input("Adjective: ")
+
+madlib = f"Hope you enjoy this video essay on {topic}. Don't forget to like and subscribe to {person}! \
+Next time we'll be covering {another_topic}, which I think you'll find {adjective}."
+
+print(madlib)
